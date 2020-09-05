4151f2b6639ea460a34810686d758981819f2ebe
Hannah Scott <hannahscott@tutanota.com>
2020-09-05 16:01:44 +0100

    added growing_tree algorithm, minor optimisation to fast_prim, added a gitignore'

--- maze-gen.py
+++ maze-gen.py
@@ -1,5 +1,6 @@
 import sys
 import time
+import argparse
 
 # print(sys.getrecursionlimit())
 sys.setrecursionlimit(10000)
@@ -7,6 +8,7 @@ sys.setrecursionlimit(10000)
 import numpy as np
 from PIL import Image
 from functions import *
+import random
 
 class MazeGenerator:
     def __init__(self, width, height):
@@ -76,6 +78,7 @@ class MazeGenerator:
         print(self.grid)
         print(frontiers)
         i = 1
+        shuffle_count = 1
 
         while len(frontiers) > 0:
             if i% 1000 == 0:
@@ -83,9 +86,13 @@ class MazeGenerator:
             i += 1
 
             # pick a random frontier point
-            np.random.shuffle(frontiers)
-            next_f = frontiers[0]
-            frontiers = frontiers[1:]
+            # if shuffle_count >= np.sqrt(len(frontiers)):
+            #     np.random.shuffle(frontiers)
+            #     shuffle_count = 1
+            idx = np.random.randint(0, len(frontiers))
+            next_f = frontiers[idx]
+            frontiers = frontiers[:idx] + frontiers[idx + 1:]
+            shuffle_count += 1
 
             # find a random nearest point
             neighbours = self.get_frontiers(next_f, value=1)
@@ -99,63 +106,80 @@ class MazeGenerator:
                 self.grid[next_n[1] + next_d[1]][next_n[0] + next_d[0]] = 1
 
                 for f in self.get_frontiers(next_f):
-                    frontiers.append(f)
-                    frontiers = list(set(frontiers))
+                    if not(f in frontiers):
+                        frontiers.append(f)
         print("* Finished in {} seconds!".format(time.time() - start))
 
+    def growing_tree(self, p):
+        start = time.time()
 
-    def prim(self):
-        wall_list = []
-        path_list = []
-        # initialize a random starting point
-        start_coord = [np.random.randint(0, self.width), np.random.randint(0, self.height)]
+        current_location = [np.random.randint(0, self.width), np.random.randint(0, self.height)]
 
-        set_element(self.grid, start_coord, 1)
-        path_list.append(start_coord)
+        directions = [
+            [0, -1],
+            [0, 1],
+            [-1, 0],
+            [1, 0]
+        ]
 
-        for n in get_neighbour_coords(self.grid, start_coord):
-            if get_element(self.grid, n) == 0:
-                wall_list.append(n)
+        cells = [current_location]
 
-        i = 0
+        self.grid[current_location[1]][current_location[0]] = 1
 
-        # prim's algorithm
-        while len(wall_list) > 0 and i < 40000:
+        i = 1
+
+        while len(cells) > 0:
+            if i% 1000 == 0:
+                print("* Step {}: {} cells with viable borders".format(i, len(cells)))
             i += 1
-            l_wall = wall_list[np.random.randint(0, len(wall_list))]
-            next_wall = np.array(l_wall)
+            # find a random nearest point
+            neighbours = self.get_frontiers(current_location)
 
-            manhattan = lambda x, y: np.abs(x[0] - y[0]) + np.abs(x[1] - y[1])
-            nearest_paths = [p for p in path_list if manhattan(p, next_wall) == 1]
+            # if there's a valid point to go to, pick one at random and go to it
+            if len(neighbours) > 0:
+                np.random.shuffle(neighbours)
+                next_n = neighbours[0]
+                next_d = [int((current_location[0] - next_n[0])/2), int((current_location[1] - next_n[1])/2)]
 
-            if len(nearest_paths) > 0:
-                path_choice = nearest_paths[np.random.randint(0, len(nearest_paths))]
-                direction = next_wall - path_choice
+                # set values for cell and passage between them
+                self.grid[next_n[1]][next_n[0]] = 1
+                self.grid[next_n[1] + next_d[1]][next_n[0] + next_d[0]] = 1
 
-                next_cell = next_wall + direction
+                # add to cells list and update location
+                cells.append(next_n)
+                current_location = self.pick_next_cell(cells, p)
+            else:
+                # current location is no longer viable
+                cells.remove(current_location)
+                # pick a new current_location unless we are done
+                if len(cells) > 0:
+                    current_location = self.pick_next_cell(cells, p)
 
-                # print(next_cell)
+        print("* Finished in {} seconds!".format(time.time() - start))
 
-                if next_cell[0] in range(self.width) and next_cell[1] in range(self.height):
+    def growing_tree_with_loops(self, p, l_p):
+        self.growing_tree(p)
 
-                    if get_element(self.grid, next_cell) != 1:
-                        set_element(self.grid, next_wall, 1)
-                        set_element(self.grid, next_cell, 1)
-                        path_list.append(next_wall)
-                        path_list.append(next_cell)
+        for i in range(len(self.grid)):
+            for j in range(len(self.grid[i])):
+                current_location = [j, i]
 
-                        for n in get_neighbour_coords(self.grid, next_cell):
-                            if get_element(self.grid, n) == 0:
-                                wall_list.append(n)
+                neighbours = self.get_frontiers(current_location)
+                if len(neighbours) == 2 and self.grid[i][j] == 0:
+                    if random.random() < l_p:
+                        self.grid[i][j] = 1
 
-                wall_list.remove(l_wall)
-
-            if i % 100 == 0:
-                print("* {}: {}".format(i, len(wall_list)))
-
-            # arr_to_png(grid, 'steps/maze_step{}.jpg'.format(i))
-
-        print("Process ended in {} steps".format(i))
+    def pick_next_cell(self, arr, prob):
+        # No point generating random numbers for the edge cases
+        if prob == 0:
+            return arr[-1]
+        elif prob == 1:
+            return arr[np.random.randint(0, len(arr))]
+        else:
+            if np.random.random() > prob:
+                return arr[-1]
+            else:
+                return arr[np.random.randint(0, len(arr))]
 
     def grid_to_png(self, fname):
         # remove top or bottom if all zeros
@@ -187,8 +211,24 @@ class MazeGenerator:
         extended_grid[-1][end] = 1
         arr_to_png(extended_grid, fname)
 
-gen = MazeGenerator(2000,2000)
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--height', '-y', type=int)
+parser.add_argument('--width', '-x', type=int)
+parser.add_argument('--probability', '-p', type=float)
+parser.add_argument('--outfile', '-o', type=str)
+
+args = parser.parse_args()
+
+gen = MazeGenerator(args.width, args.height)
 # gen.prim()
 # gen.grid_to_png('maze.png')
-gen.fast_prim()
-gen.grid_to_png('fast-prim.png')
+# gen.fast_prim()
+# gen.grid_to_png('fast-prim.png')
+#
+# gen = MazeGenerator(200, 200)
+# gen.recursive_backtracking()
+# gen.grid_to_png('recursive-backtracking.png')
+
+gen.growing_tree(args.probability)
+gen.grid_to_png(args.outfile)
